147f97d0e7a5e95be8f4a056fc56964a762879ca
rachel <rachelyk11@gmail.com>
2023-05-28 00:33:55 +1000

    migrated to typescript and everything is break and i go to sleep now

--- my-app/src/App.ts
+++ my-app/src/App.ts
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
@@ -42,29 +42,6 @@ function InputSpending() {
   )
 }
 
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 export default InputSpending;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
